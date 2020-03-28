d02584c5e071ddde99f4a3ae54e9c0105605ae05
sinyun <yunyumyun543@gmail.com>
2020-03-28 22:25:15 +0900

    state 3.3

--- src/App.js
+++ src/App.js
@@ -3,91 +3,26 @@ import React from 'react';
 
 
 class App extends React.Component{
-  // state = {
-  //   count: 0
-  // };
-  // add = () => {
-  //   this.setState(current => ({count: current.count +1 }));
-  // };
-  // minus = () => {
-  //   this.setState(current => ({count: current.count -1}));
-  // };
-  // componentDidMount(){
-  //   console.log("component rendered");
-  // }
-  // componentDidUpdate(){
-  //   console.log("component update");
-  // }
-  
-  // // componentWillUnmount(){
-  // //   console.log("goob bye");
-  // // }
-  // render() {
-  //   console.log("I'm rendering")
-  //   return (
-  //     <div>
-  //       <h1>The number is : {this.state.count}</h1>
-  //       <button onClick={this.add}>Add</button>
-  //       <button onClick={this.minus}>Minus</button>
-  //     </div>
-  //   );
-  // }
+ 
+  state = {
+    isLoading: true,
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000)
+  }
+
+  render() {
+    // const {isLoading} = this.state;
+    return (
+      <div>
+        {this.state.isLoading ? " Loading" : "We are ready"}
+      </div>
+    );
+  }
 }
-// function Food({name, picture, rating}){
-//   return (
-//     <div>
-//       <h1>I like {name}</h1>
-//       {/*
-//         alt is a function for the visually impaired,
-//         and React is recommeded to use it.
-//        */}
-//       <img src={picture} alt={name}/>
-//       <h4>{rating}/5</h4>
-//     </div>
-//   );
-// }
 
-// Food.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   picture: PropTypes.string.isRequired,
-//   rating: PropTypes.number.isRequired
-// };
-
-// const foodList = [
-//   {
-//     id : 1,
-//     name : "Kimchi",
-//     image : "https://m.gungangmall.co.kr/web/product/big/201607/861_shop1_630241.png",
-//     rating : 5
-//   },
-//   {
-//     id : 2,
-//     name : "Sagyeopsal",
-//     image : "https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200",
-//     rating : 4.8
-//   }
-// ]
-
-// // function renderFood(dish){
-// //   return <Food name={dish.name} picture={dish.image} />
-// // }
-// // This is the same code as above.
-// // {foodList.map( dish => (<Food name={dish.name} picture={dish.image} />))}
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* dish is foodList Obejct.
-//           If you don't define a key value when using a list in JavaScript, 
-//           you can see that there is an error in the web browser console.
-//           However, it is not a problem for execution.
-//           This key allows uniqueness as the index number of a list object.
-//       */}
-//       {foodList.map( dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
-//     </div>
-//   );
-// }
 
 export default App;
