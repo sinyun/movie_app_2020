cba1db3ba7abb43677a5ba7335f848d241030961
sinyun <yunyumyun543@gmail.com>
2020-03-29 13:54:30 +0900

    Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./component/Navigation"
 
-
-class App extends React.Component{
- 
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    const {data: { data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    // first movies object is state, second movies object is axios
-    // this.setState({movies:movies});
-    this.setState({movies, isLoading : false});
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-      <selection className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-          ) : ( 
-            <div className="movies">
-              {movies.map( movie => (
-                <Movie 
-                    key={movie.id} 
-                    id={movie.id}
-                    year={movie.year} 
-                    title={movie.title} 
-                    summary={movie.summary} 
-                    poster={movie.medium_cover_image} 
-                    genres={movie.genres}
-                  />
-              ))}
-            </div>
-        )}
-      </selection>
-    );
-  }
-
+function App(){
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route paht="/" exact={true} component={Home} />        
+      <Route paht="/about" component={About} />
+    </HashRouter>
+  );
 }
-
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,56 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from '../component/Movie';
+import "./Home.css";
+
+
+class Home extends React.Component{
+ 
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const {data: { data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // first movies object is state, second movies object is axios
+    // this.setState({movies:movies});
+    this.setState({movies, isLoading : false});
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <selection className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+          ) : ( 
+            <div className="movies">
+              {movies.map( movie => (
+                <Movie 
+                    key={movie.id} 
+                    id={movie.id}
+                    year={movie.year} 
+                    title={movie.title} 
+                    summary={movie.summary} 
+                    poster={movie.medium_cover_image} 
+                    genres={movie.genres}
+                  />
+              ))}
+            </div>
+        )}
+      </selection>
+    );
+  }
+
+}
+
+
+export default Home;
