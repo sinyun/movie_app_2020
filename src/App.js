ec640c9e1c2bf3e422d686d30606752fb309d5d9
sinyun <yunyumyun543@gmail.com>
2020-03-29 14:35:42 +0900

    last_movie_2020

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail"
 import Navigation from "./component/Navigation"
+import "./App.css";
 
 function App(){
   return (
@@ -10,6 +12,7 @@ function App(){
       <Navigation />
       <Route paht="/" exact={true} component={Home} />        
       <Route paht="/about" component={About} />
+      <Route paht="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
